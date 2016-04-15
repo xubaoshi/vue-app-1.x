930508bc34fd50b01a129fd6c588cdc2944474c5
xubaoshi <948869516@qq.com>
2016-04-15 16:56:59 +0800

    add vue plugin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
  * Created by xu on 2016/4/13.
  */
 //https://blog.madewithlove.be/post/webpack-your-bags/
+import Vue from 'vue'
+import store from './vuex/store'
+
+
 import './style/css/base.css'
 import './style/css/reset.css'
 alert("ahh")
---
+++ src/vuex/store.js
@@ -0,0 +1,3 @@
+/**
+ * Created by xubaoshi on 2016/4/15.
+ */
